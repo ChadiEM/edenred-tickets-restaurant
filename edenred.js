ad482f4adfc7d77e0e697e146c75be4ffd148e04
Chadi El Masri <chadimsr@gmail.com>
2022-12-19 16:19:31 +0100

    Reformat code.

--- edenred.js
+++ edenred.js
@@ -1,37 +1,43 @@
 // Sorry for the horrible code.
 
-const puppeteer = require('puppeteer-core');
-const fs = require('fs');
-const express = require('express')
-const winston = require('winston');
+const puppeteer = require("puppeteer-core");
+const fs = require("fs");
+const express = require("express");
+const winston = require("winston");
 
-const COOKIES_FILE = process.env['EDENRED_COOKIE_FILE'] != null ? process.env['EDENRED_COOKIE_FILE'] : '/tmp/.edenred_cookies'
-const UDPATE_INTERVAL = process.env['EDENRED_UPDATE_INTERVAL'] != null ? process.env['EDENRED_UPDATE_INTERVAL'] : 300000
-const PORT = 8080
+const COOKIES_FILE =
+    process.env["EDENRED_COOKIE_FILE"] != null
+        ? process.env["EDENRED_COOKIE_FILE"]
+        : "/tmp/.edenred_cookies";
+const UDPATE_INTERVAL =
+    process.env["EDENRED_UPDATE_INTERVAL"] != null
+        ? process.env["EDENRED_UPDATE_INTERVAL"]
+        : 300000;
+const PORT = 8080;
 
 const logger = winston.createLogger({
-    level: 'info',
+    level: "info",
     format: winston.format.combine(
         winston.format.timestamp(),
         winston.format.json()
     ),
-    transports: [
-        new winston.transports.Console()
-    ],
+    transports: [new winston.transports.Console()],
 });
 
-logger.info('Starting edenred updater - UDPATE_INTERVAL=' + UDPATE_INTERVAL + ', COOKIES_FILE=' + COOKIES_FILE + ', PORT=' + PORT +'.');
+logger.info(
+    `Starting edenred updater - UDPATE_INTERVAL=${UDPATE_INTERVAL}, COOKIES_FILE=${COOKIES_FILE}, PORT=${PORT}.`
+);
 
-const app = express()
+const app = express();
 
 let trValues = {
     total: 0,
     today: 0,
-    lastUpdated: 0
-}
+    lastUpdated: 0,
+};
 
 app.get("/tr", (_req, res) => {
-    if (trValues['lastUpdated'] == 0) {
+    if (trValues["lastUpdated"] == 0) {
         res.sendStatus(404);
     } else {
         res.status(200).json(trValues);
@@ -39,23 +45,23 @@ app.get("/tr", (_req, res) => {
 });
 
 app.get("/health", (_req, res) => {
-	const healthcheck = {
+    const healthcheck = {
         message: "UP",
-		uptime: process.uptime(),
-		timestamp: Date.now()
-	};
+        uptime: process.uptime(),
+        timestamp: Date.now(),
+    };
     res.status(200).json(healthcheck);
 });
 
 update();
 
 const updater = setInterval(() => update(), UDPATE_INTERVAL);
-const server = app.listen(PORT)
+const server = app.listen(PORT);
 
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
     clearInterval(updater);
     server.close();
-})
+});
 
 async function injectCookies(page) {
     return new Promise((resolve) => {
@@ -73,9 +79,9 @@ async function injectCookies(page) {
 
 async function saveCookies(page) {
     const client = await page.target().createCDPSession();
-    let cookies = (await client.send("Network.getAllCookies"));
+    let cookies = await client.send("Network.getAllCookies");
     return new Promise((resolve, reject) => {
-        let data = JSON.stringify(cookies['cookies']);
+        let data = JSON.stringify(cookies["cookies"]);
         logger.info("Writing cookies.");
         fs.writeFile(COOKIES_FILE, data, (err) => {
             if (err) {
@@ -88,24 +94,32 @@ async function saveCookies(page) {
 }
 
 async function update() {
-    const browser = await puppeteer.launch({ headless: true, slowMo: 25, timeout: 300000, executablePath: '/usr/bin/chromium-browser' });
+    const browser = await puppeteer.launch({
+        headless: true,
+        slowMo: 25,
+        timeout: 300000,
+        executablePath: "/usr/bin/chromium-browser",
+    });
 
     try {
-        logger.info('Begin update.')
+        logger.info("Begin update.");
 
-        if (process.env['EDENRED_COOKIE'] != null && !fs.existsSync(COOKIES_FILE)) {
+        if (
+            process.env["EDENRED_COOKIE"] != null &&
+            !fs.existsSync(COOKIES_FILE)
+        ) {
             logger.info("Reading provided cookie from env var.");
-            fs.writeFile(COOKIES_FILE, process.env['EDENRED_COOKIE'], (err) => {
+            fs.writeFile(COOKIES_FILE, process.env["EDENRED_COOKIE"], (err) => {
                 if (err) {
-                    logger.error('Cannot write initial cookie.')
+                    logger.error("Cannot write initial cookie.");
                 }
             });
         }
 
         const page = await browser.newPage();
 
-        page.on('error', err => {
-            logger.error('Page error: ' + err);
+        page.on("error", (err) => {
+            logger.error("Page error: " + err);
             browser.close();
         });
 
@@ -116,37 +130,48 @@ async function update() {
 
         // needed because headless mode doesn't send them
         await page.setExtraHTTPHeaders({
-            'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
+            "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
         });
 
         // needed because headless mode sends HeadlessChrome
-        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36')
+        await page.setUserAgent(
+            "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36"
+        );
 
-        await page.goto('https://www.myedenred.fr/carte/')
+        await page.goto("https://www.myedenred.fr/carte/");
 
-        let isLoggedIn = false
+        let isLoggedIn = false;
         try {
-            await page.waitForSelector('div.carte-body > div.carte-body-left > span.carte-body_solde', { visible: true, timeout: 30000 });
-            isLoggedIn = true
+            await page.waitForSelector(
+                "div.carte-body > div.carte-body-left > span.carte-body_solde",
+                { visible: true, timeout: 30000 }
+            );
+            isLoggedIn = true;
         } catch (err) {
-            logger.info('Cannot see card values right away.')
+            logger.info("Cannot see card values right away.");
         }
 
         if (!isLoggedIn) {
             try {
-                await page.waitForSelector('#Username', { visible: true, timeout: 120000 });
-                await page.type('#Username', process.env['EDENRED_USER'])
-                await page.type('#Password', process.env['EDENRED_PASS'])
-                await page.click('#RememberLogin')
-                await page.click('#login')
-                await page.waitForNavigation({ waitUntil: 'networkidle2' });
+                await page.waitForSelector("#Username", {
+                    visible: true,
+                    timeout: 120000,
+                });
+                await page.type("#Username", process.env["EDENRED_USER"]);
+                await page.type("#Password", process.env["EDENRED_PASS"]);
+                await page.click("#RememberLogin");
+                await page.click("#login");
+                await page.waitForNavigation({ waitUntil: "networkidle2" });
             } catch (err) {
-                logger.info('Possibly already logged in, skipping login step.')
+                logger.info("Possibly already logged in, skipping login step.");
             }
         }
 
         try {
-            await page.waitForSelector('div.carte-body > div.carte-body-left > span.carte-body_solde', { visible: true, timeout: 300000 });
+            await page.waitForSelector(
+                "div.carte-body > div.carte-body-left > span.carte-body_solde",
+                { visible: true, timeout: 300000 }
+            );
         } catch (err) {
             logger.info("Selector didn't show up. Skipping update.");
             return;
@@ -154,25 +179,30 @@ async function update() {
 
         await saveCookies(page);
 
-        logger.info('Attempting to read data...');
+        logger.info("Attempting to read data...");
 
-        let remaining = '--';
-        let today = '--';
+        let remaining = "--";
+        let today = "--";
 
-        while (remaining.includes('--') || today.includes('--')) {
-            remaining = await page.$eval('div.carte-body > div.carte-body-right > span.carte-body_solde', el => el.innerText);
-            today = await page.$eval('div.carte-body > div.carte-body-left > span.carte-body_solde', el => el.innerText);
+        while (remaining.includes("--") || today.includes("--")) {
+            remaining = await page.$eval(
+                "div.carte-body > div.carte-body-right > span.carte-body_solde",
+                (el) => el.innerText
+            );
+            today = await page.$eval(
+                "div.carte-body > div.carte-body-left > span.carte-body_solde",
+                (el) => el.innerText
+            );
         }
 
         logger.info(`Remaining = ${remaining}, Today = ${today}`);
 
-        trValues['total'] = parseFloat(remaining.split(' ')[0]);
-        trValues['today'] = parseFloat(today.split(' ')[0]);
-        trValues['lastUpdated'] = Date.now();
+        trValues["total"] = parseFloat(remaining.split(" ")[0]);
+        trValues["today"] = parseFloat(today.split(" ")[0]);
+        trValues["lastUpdated"] = Date.now();
 
-        logger.info('End update.')
+        logger.info("End update.");
     } finally {
         await browser.close();
     }
 }
-
